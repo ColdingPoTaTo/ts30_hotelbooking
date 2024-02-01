242d706fe4bb479466337c40b2f87b468e2002fc
William <william.9150@gmail.com>
2024-02-01 23:37:56 +0800

    NODE_ENV

--- src/utils/useHelper.ts
+++ src/utils/useHelper.ts
@@ -1,18 +1,18 @@
-import { useDisplay } from "vuetify"
+import { useDisplay } from "vuetify";
 import { img_host } from "@/utils/imageSource";
-import { computed } from "vue"
-export function useHelper(){
-  const { name } = useDisplay()
-  const webOrMobile = computed(()=>{
-    if(name.value==='xs') return 'mobile'
-    else return 'web'
-  })
-  const getImageUrl = (fileName:string):string=>{
-    return `/imgs/${img_host[webOrMobile.value]}/${fileName}`
-  }
+import { computed } from "vue";
+export function useHelper() {
+  const { name } = useDisplay();
+  const webOrMobile = computed(() => {
+    if (name.value === "xs") return "mobile";
+    else return "web";
+  });
+  const getImageUrl = (fileName: string): string => {
+    return `/imgs/${img_host[webOrMobile.value]}/${fileName}`;
+  };
 
-  return{
+  return {
     webOrMobile,
-    getImageUrl
-  }
+    getImageUrl,
+  };
 }
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || "/",
+  base: process.env.NODE_ENV === "production" ? "/ts30_hotelbooking/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
